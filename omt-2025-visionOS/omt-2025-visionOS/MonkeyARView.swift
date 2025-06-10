d344989451b32cbb7dab0f3e9f18e9190d84e64b
John Forester <forester.john@gmail.com>
2025-06-09 21:58:50 -0700

    add idle animation to AR monkey

--- omt-2025-visionOS/omt-2025-visionOS/MonkeyARView.swift
+++ omt-2025-visionOS/omt-2025-visionOS/MonkeyARView.swift
@@ -7,14 +7,20 @@
 
 import SwiftUI
 import RealityKit
+import RealityKitContent
 
 struct MonkeyARView: View {
     var body: some View {
         RealityView { content in
-            // JF: TODO
-            
-            // Challenges:
-            
+            if let scene = try? await Entity(named: "ARScene", in: realityKitContentBundle) {
+                content.add(scene)
+
+                if let root = scene.children.first,
+                   let animationLibrary = root.components[AnimationLibraryComponent.self],
+                   let animationResource = animationLibrary.defaultAnimation {
+                    root.playAnimation(animationResource, transitionDuration: 0, startsPaused: false)
+                }
+            }
         }
     }
 }
